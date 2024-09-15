3b9a49b56114987feafd0476b2837bb3e7475119
hisanari <hisanari.1987@gmail.com>
2024-09-15 14:31:28 +0900

    add constants

---
+++ common.h
@@ -0,0 +1,13 @@
+//
+// Created by hm on 24/09/14.
+//
+
+#ifndef clox_common_h
+#define clox_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+
+#endif
---
+++ debug.h
@@ -0,0 +1,14 @@
+//
+// Created by hm on 24/09/15.
+//
+
+#ifndef clox_debug_h
+#define clox_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
+
